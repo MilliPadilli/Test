fdf7ca1a54ff611e4318d8a508fe281862ae9406
MilliPadilli <emilia-padilla@hotmail.com>
2025-01-16 00:54:59 +0100

    first commit

---
+++ src/main/java/com/example/testafeksamen/TestafeksamenApplication.java
@@ -0,0 +1,13 @@
+package com.example.testafeksamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestafeksamenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestafeksamenApplication.class, args);
+    }
+
+}
